6ffd860adf9d981708eedadfa2c82cada1f01537
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2026-07-13 21:51:51 +0530

    Time: 3 ms (45.32%), Space: 10.2 MB (76.74%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int k=0;
+        int l=0;
+        for(int i=0;i<moves.size();i++)
+        {
+            if(moves[i]=='L')
+            {
+                l++;
+            }
+            if(moves[i]=='R')
+            {
+                l--;
+            }
+           if(moves[i]=='U')
+           {
+            k++;
+           }
+           if(moves[i]=='D')
+           {
+            k--;
+           }
+        }
+        if(k!=0 || l!=0)
+        return false;
+        else
+        return true;
+    }
+};
