51e00fd0e8477e42ce7c7b6428f1c0180d048251
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2026-07-21 21:25:07 +0530

    Time: 79 ms (37.21%), Space: 164.7 MB (18.19%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int pallength(string s,int l,int r)
+{
+    while(l>=0 && r<s.size() && s[l]==s[r])
+    {
+        l--;
+        r++;
+    }
+    return r-l-1;
+}
+    string longestPalindrome(string s) {
+        int st=0;
+        int m=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int n1=pallength(s,i,i);
+            int n2=pallength(s,i,i+1);
+            int n=max(n1,n2);
+            if(n>m)
+            {
+                 m=n;
+                st=i-(n-1)/2;
+            }
+        }
+        return s.substr(st,m);
+    }
+};
