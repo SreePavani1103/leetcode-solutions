a7ad49ea11a08d033ff3f0338829070e609eb47e
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2026-08-25 15:18:43 +0530

    Time: 11 ms (56.99%), Space: 22.8 MB (53.64%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -1,41 +1,22 @@
-bool is_prime(int n)
-{
-    if(n<=1)
-    return false;
-    for(int i=2;i*i<=n;i++)
-    {
-        if(n%i==0)
-        return false;
-    }
-    return true;
-}
-vector<int> prime_fact(int n)
-{
-    vector<int>v;
-    for(int i=1;i<=n;i++)
-    {
-        if(n%i==0)
-        {
-            if(is_prime(i))
-            {
-                v.push_back(i);
-            }
-        }
-    }
-    return v;
-}
 class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
-      vector<vector<int>>v;
-        for(int i=0;i<nums.size();i++)
-        {
-             v.push_back(prime_fact(nums[i]));
-        }
+        int n=nums.size();
         set<int>s;
-        for(int i=0;i<v.size();i++)
+        for(int x:nums)
         {
-            s.insert(v[i].begin(),v[i].end());
+            int k=x;
+            for(int i=2;i*i<=k;i++)
+            {
+                if(k%i==0)
+                {
+                    s.insert(i);
+                    while(k%i==0)
+                     k=k/i;
+                }
+            }
+            if(k>1)
+            s.insert(k);
         }
         return s.size();
     }
